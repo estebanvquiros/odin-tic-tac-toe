fbdc67caf687f887583dbefcd758e0c6798a6725
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-12-07 16:41:36 -0600

    Add module to manage the board
    Implements board initialization and mark placement, providing a
    consistent structure for creating and updating the board state.

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log("Tic-Tac-Toe");
+
+const BOARD_SIZE = 9;
+
+const gameBoard = (function () {
+	const board = [];
+
+	function initBoard() {
+		for (let i = 0; i < BOARD_SIZE; i++) {
+			board.push(0);
+		}
+		_printBoard();
+	}
+
+	function placeMark(position, mark) {
+		if (!_isValidPosition(position) || !_isPositionAvailable(position)) {
+			console.log("[ERROR]: Position is not available or out of range.");
+			return false;
+		}
+		board[position] = mark;
+		_printBoard();
+		return true;
+	}
+
+	function _isValidPosition(position) {
+		return position >= 0 && position < board.length;
+	}
+
+	function _isPositionAvailable(position) {
+		return board[position] === 0;
+	}
+
+	function _printBoard() {
+		// Only for testing purposes
+		let result = "";
+		for (let i = 0; i < BOARD_SIZE; i++) {
+			if (i !== 0 && i % 3 === 0) result += "\n";
+			result += `  ${board[i]}`;
+		}
+		console.log(result);
+	}
+
+	return { initBoard, placeMark };
+})();
